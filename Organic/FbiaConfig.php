9d577e2d7fb7d0e749733f3ff650b23b4d845b59
Stanislav Glebov <stanislav.glebov@ivelum.com>
2022-03-01 20:02:21 +0500

    EP-1521: PageInjection fbia-related code

--- Organic/FbiaConfig.php
+++ Organic/FbiaConfig.php
@@ -7,6 +7,8 @@ class FbiaConfig extends BaseConfig {
     const MODE_AUTOMATIC = 1;
     const MODE_MANUAL = 2;
 
+    const FB_AD_SOURCE_NONE = 'none';
+
     /**
      * Map (key -> prefill) of prefills for placements returned from Organic Platform API
      * Each prefill must contain at least:
@@ -21,6 +23,21 @@ class FbiaConfig extends BaseConfig {
         parent::__construct( $raw );
         $this->mode = self::validMode( $raw['mode'] );
         $this->enabled = $raw['enabled'] ?? false;
+        if ( ! $this->enabled ) {
+            return;
+        }
+
+        if ( ! class_exists( 'Instant_Articles_Post' ) || empty( $this->forPlacement ) ) {
+            $this->enabled = false;
+            return;
+        }
+
+        $settings_ads = \Instant_Articles_Option_Ads::get_option_decoded() ?? [];
+        $ad_source = isset( $settings_ads['ad_source'] ) ? $settings_ads['ad_source'] : self::FB_AD_SOURCE_NONE;
+
+        if ( $ad_source !== self::FB_AD_SOURCE_NONE ) {
+            $this->enabled = false;
+        }
     }
 
     public static function validMode( int $mode ) {
