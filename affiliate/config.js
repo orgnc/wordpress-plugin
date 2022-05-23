b061da09ea945e97442d9f92976bd0ab452a4995
Rustam Kashapov <rustam.kashapov@ivelum.com>
2022-05-23 17:41:29 +0300

    EP-2009 Dynamic affiliate sdk config

--- affiliate/config.js
+++ affiliate/config.js
@@ -2,13 +2,12 @@ window.empire ||= {};
 window.empire.apps ||= {};
 window.empire.apps.affiliate ||= {};
 window.empire.apps.affiliate.config = {
-  // TODO rkashapov: implement dynamic configuration
   siteConf: {
-    guid: 'f5cdaf40-6928-412d-8a20-fa09ac1a22a1',
-    publicDomain: 'https://organic.betteryoumag.com',
+    guid: organic_sdk_config.siteGuid,
+    publicDomain: organic_sdk_config.publicDomain,
   },
   appConf: {
-    linkDomains: ['https://betteryoumag-com.organicly.io', 'https://organic.betteryoumag.com'],
+    linkDomains: organic_sdk_config.linkDomains,
     trackingIDs: {},
   }
 };
