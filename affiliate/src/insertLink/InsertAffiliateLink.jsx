e17781fc35c81437758d965e924a698f8a43fba5
Rustam Kashapov <rustam.kashapov@ivelum.com>
2022-05-24 21:00:52 +0300

    EP-2008 Gutenberg: Affiliate Insert Link

---
+++ affiliate/src/insertLink/InsertAffiliateLink.jsx
@@ -0,0 +1,96 @@
+import { BlockControls } from '@wordpress/block-editor';
+import { ToolbarDropdownMenu } from '@wordpress/components';
+import { useCallback, useState } from '@wordpress/element';
+import {
+  toggleFormat, registerFormatType, removeFormat, create, insert, applyFormat,
+} from '@wordpress/rich-text';
+import PropTypes from 'prop-types';
+
+import OrganicIcon from '../OrganicIcon';
+import ProductSearchModal from '../ProductSearchModal';
+
+const FORMAT_NAME = 'organic/affiliate-product-link';
+
+const InsertAffiliateLink = ({ isActive, onChange, value }) => {
+  const [showModal, setShowModal] = useState(false);
+  const hideModal = useCallback(
+    () => setShowModal(false),
+    [setShowModal],
+  );
+  const removeLink = useCallback(
+    () => {
+      onChange(removeFormat(value, FORMAT_NAME));
+    },
+    [onChange, value],
+  );
+  const displayModal = useCallback(
+    () => setShowModal(true),
+    [setShowModal],
+  );
+  const onProductSelect = useCallback((product) => {
+    const { start, end } = value;
+    const format = {
+      type: FORMAT_NAME,
+      attributes: { url: product.link },
+    };
+    let newValue;
+    if (start === end) {
+      // If there is no selection then insert product name and add the link
+      const productName = create({ text: product.name });
+      newValue = insert(
+        value,
+        applyFormat(productName, format, 0, product.name.length),
+        start,
+        end,
+      );
+    } else {
+      newValue = toggleFormat(value, format);
+    }
+    onChange(newValue);
+    hideModal();
+  }, [value, onChange, hideModal]);
+  return (
+    <>
+      <BlockControls group="other">
+        <ToolbarDropdownMenu
+          controls={[
+            {
+              title: isActive ? 'Remove Affiliate Link' : 'Insert Affiliate Link',
+              icon: 'admin-links',
+              onClick: isActive ? removeLink : displayModal,
+            },
+          ]}
+          icon={OrganicIcon}
+          label="Organic Tools"
+        />
+      </BlockControls>
+      {showModal
+        && (
+        <ProductSearchModal
+          onClose={hideModal}
+          onProductSelect={onProductSelect}
+        />
+        )}
+    </>
+  );
+};
+
+InsertAffiliateLink.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.shape({
+    start: PropTypes.number,
+    end: PropTypes.number,
+  }).isRequired,
+  isActive: PropTypes.bool.isRequired,
+};
+
+const register = () => registerFormatType(FORMAT_NAME, {
+  title: 'Affiliate Product Link',
+  tagName: 'a',
+  attributes: { url: 'href' },
+  edit: InsertAffiliateLink,
+});
+
+export {
+  register,
+};
