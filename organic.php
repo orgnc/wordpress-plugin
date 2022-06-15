c4fb5f7b2fad70f2ed5173be03013352bb40a195
Sergey Anuchin <sergunich@gmail.com>
2022-06-15 14:46:59 +0300

    Fix config env vars passing to the init

--- organic.php
+++ organic.php
@@ -23,4 +23,4 @@ if ( ! $environment ) {
 }
 
 $organic = new Organic( $environment );
-$organic->init( getenv( 'EMPIRE_API_URL', getenv( 'EMPIRE_CDN_URL' ) ) );
+$organic->init( getenv( 'EMPIRE_API_URL' ), getenv( 'EMPIRE_CDN_URL' ) );
