fa9787f399afea109a67fec73be11530b2293799
Rustam Kashapov <rustam.kashapov@ivelum.com>
2022-07-01 11:03:12 +0300

    Remove not-allowed product card options

--- affiliate/src/index.js
+++ affiliate/src/index.js
@@ -24,34 +24,14 @@ registerBlockType('organic/affiliate-product-card', {
     },
     displayDescription: {
       type: 'boolean',
-      default: true,
-    },
-    cardRadius: {
-      type: 'boolean',
-      default: true,
-    },
-    cardShadow: {
-      type: 'boolean',
-      default: true,
-    },
-    textColor: {
-      type: 'string',
-      default: '#212529',
-    },
-    linkColor: {
-      type: 'string',
-      default: '#13A93A',
-    },
-    backgroundColor: {
-      type: 'string',
-      default: '#FFFFFF',
+      default: false,
     },
   },
   example: {
     attributes: {
       productGuid: '964805c4-6bf7-4418-a112-a58f1565a72d',
       displayImage: true,
-      displayDescription: true,
+      displayDescription: false,
     },
   },
   icon: OrganicIcon,
