497ada29a9497d0d4af972208a6bf26019657fba
Stanislav Glebov <stanislav.glebov@ivelum.com>
2022-08-16 13:49:59 +0500

    EP-2892: amp-connatix-player aspect ratio

--- Organic/types.php
+++ Organic/types.php
@@ -6,3 +6,18 @@ class AD_TYPE {
     const DEFAULT = 'display_default';
     const OUTSTREAM_VIDEO = 'outstream_video';
 }
+
+class AMP_BREAKPOINT {
+    const SM = 576;
+    const MD = 768;
+    const LG = 992;
+    const XL = 1200;
+
+    public static function minWidth( int $size ) {
+        return "(min-width: ${size}px)";
+    }
+
+    public static function maxWidth( int $size ) {
+        return "(max-width: ${size}px)";
+    }
+}
