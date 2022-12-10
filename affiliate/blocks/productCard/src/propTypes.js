3572650cfd934b006e762e63b79ec062ed2576e9
Mwindo <christopher.bisom@organic.ly>
2022-12-10 08:21:05 -0500

    remove accidental import

--- affiliate/blocks/productCard/src/propTypes.js
+++ affiliate/blocks/productCard/src/propTypes.js
@@ -1,4 +1,4 @@
-import PropTypes, {checkPropTypes} from 'prop-types';
+import PropTypes from 'prop-types';
 
 export const AttributesType = PropTypes.shape({
   bannerText: PropTypes.string.isRequired,
