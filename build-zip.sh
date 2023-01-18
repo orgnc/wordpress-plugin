9631c16e9d018e1a84ab523b791529affe6028ab
iceone <iceone@yandex.ru>
2023-01-18 12:48:50 +0500

    EP-2249: use liborm85/composer-vendor-cleaner for cleaning dev files from vendor

--- build-zip.sh
+++ build-zip.sh
@@ -1,20 +1,38 @@
 #!/bin/bash -e
-BUILD_NUMBER=$1;
+BUILD_NUMBER=${1:-"dev"}
+ROOT_DIR=$(pwd)
+rm -rf ./build/*
 mkdir -p build/organic
 ls -alh
-sudo chown -R runner:docker .
+
+if ! [[ $BUILD_NUMBER == "dev" ]]; then
+  sudo chown -R runner:docker .
+fi
+
 cp -r Organic build/organic/
 cp organic.php build/organic/
 cp readme.txt build/organic/
-
-sed -i "s/ORGANIC_PLUGIN_VERSION_VALUE/${BUILD_NUMBER}/" build/organic/organic.php build/organic/readme.txt
+# macos sed has different options
+if [[ $(uname) == 'Darwin' ]]; then
+  sed -i '' "s/ORGANIC_PLUGIN_VERSION_VALUE/${BUILD_NUMBER}/" build/organic/organic.php build/organic/readme.txt
+else
+  sed -i "s/ORGANIC_PLUGIN_VERSION_VALUE/${BUILD_NUMBER}/" build/organic/organic.php build/organic/readme.txt
+fi
 
 cp composer.json build/organic/
 cp composer.lock build/organic/
-cp -r vendor build/organic/
+
+cd build/organic
+composer install --no-dev
+cd $ROOT_DIR
+
+# cp -r vendor build/organic/
 (cd affiliate/ && npm ci && npm run build)
-mkdir -p build/organic/affiliate/
-cp -r affiliate/blocks/ build/organic/affiliate/
+mkdir -p build/organic/affiliate/productCard build/organic/affiliate/productCarousel
+cp -r affiliate/blocks/productCard/build build/organic/affiliate/productCard/
+cp affiliate/blocks/productCard/block.json build/organic/affiliate/productCard/
+cp -r affiliate/blocks/productCarousel/build build/organic/affiliate/productCarousel/
+cp affiliate/blocks/productCarousel/block.json build/organic/affiliate/productCarousel/
 cd build
 zip -r organic-${BUILD_NUMBER}.zip organic
 cp organic-${BUILD_NUMBER}.zip organic.zip
