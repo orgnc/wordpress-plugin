4c67f7f2331e42d23b04f1235128dd7021138b11
iceone <iceone@yandex.ru>
2023-02-21 15:46:43 +0500

    EP-4788: Escape Organic->getCurrentUrl() value

--- Organic/Organic.php
+++ Organic/Organic.php
@@ -493,8 +493,8 @@ class Organic {
     }
 
     public function getCurrentUrl() {
-        $protocol = is_ssl() ? 'https://' : 'http://';
-        return $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        $scheme = is_ssl() ? 'https' : 'http';
+        return esc_url( $scheme . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
     }
 
     public function getTagsFor( $postId ) {
