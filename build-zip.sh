c15f5c6bafa58c3970a9989d87e799b3cecc55d4
Sergey Anuchin <sergunich@gmail.com>
2023-03-26 20:19:33 +0300

    EP-4897: move all sources into src/ dir

--- build-zip.sh
+++ build-zip.sh
@@ -9,9 +9,9 @@ if ! [[ $BUILD_NUMBER == "dev" ]]; then
   sudo chown -R runner:docker .
 fi
 
-cp -r Organic build/organic/
-cp organic.php build/organic/
-cp readme.txt build/organic/
+cp -r src/Organic build/organic/
+cp src/organic.php build/organic/
+cp src/readme.txt build/organic/
 # macos sed has different options
 if [[ $(uname) == 'Darwin' ]]; then
   sed -i '' "s/ORGANIC_PLUGIN_VERSION_VALUE/${BUILD_NUMBER}/" build/organic/organic.php build/organic/readme.txt
@@ -19,21 +19,21 @@ else
   sed -i "s/ORGANIC_PLUGIN_VERSION_VALUE/${BUILD_NUMBER}/" build/organic/organic.php build/organic/readme.txt
 fi
 
-cp composer.json build/organic/
-cp composer.lock build/organic/
+cp src/composer.json build/organic/
+cp src/composer.lock build/organic/
 
 cd build/organic
 composer install --no-dev
 cd $ROOT_DIR
 
 # cp -r vendor build/organic/
-(cd affiliate/ && npm ci && npm run build)
+(cd src/affiliate/ && npm ci && npm run build)
 mkdir -p build/organic/affiliate/blocks/productCard build/organic/affiliate/blocks/productCarousel
-cp -r affiliate/blocks/productCard/build build/organic/affiliate/blocks/productCard/
-cp affiliate/blocks/productCard/block.json build/organic/affiliate/blocks/productCard/
-cp -r affiliate/blocks/productCarousel/build build/organic/affiliate/blocks/productCarousel/
-cp affiliate/blocks/productCarousel/block.json build/organic/affiliate/blocks/productCarousel/
-cp affiliate/initSDKOnPostLoad.js build/organic/affiliate/
+cp -r src/affiliate/blocks/productCard/build build/organic/affiliate/blocks/productCard/
+cp src/affiliate/blocks/productCard/block.json build/organic/affiliate/blocks/productCard/
+cp -r src/affiliate/blocks/productCarousel/build build/organic/affiliate/blocks/productCarousel/
+cp src/affiliate/blocks/productCarousel/block.json build/organic/affiliate/blocks/productCarousel/
+cp src/affiliate/initSDKOnPostLoad.js build/organic/affiliate/
 cd build
 zip -r organic-${BUILD_NUMBER}.zip organic
 cp organic-${BUILD_NUMBER}.zip organic.zip
