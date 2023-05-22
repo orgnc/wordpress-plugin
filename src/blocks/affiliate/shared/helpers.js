e50a35128c6837a24793a642a8446eabeaee2bfb
Mwindo <christopher.bisom@organic.ly>
2023-05-22 13:56:53 -0400

    see if I can remove hacky setTimeout now that I've identified some of the issues had to do with caching

--- src/blocks/affiliate/shared/helpers.js
+++ src/blocks/affiliate/shared/helpers.js
@@ -25,10 +25,6 @@ export const refreshAffiliateWidgetsOnEdit = () => {
 };
 
 export const refreshAffiliateWidgetsOnSave = () => {
-  // Ideally, we wait for the Gutenberg editor to "be ready" and then refresh the display,
-  // rendering our widget divs as iframed content. However, this has been difficult to
-  // get working across browsers and WP environments.
-  // TODO: Improve this!
   const refreshWidgetsIfNecessary = () => {
     // Each widget on the page will call this, so we put a check to only refresh once.
     // eslint-disable-next-line no-underscore-dangle
@@ -38,8 +34,6 @@ export const refreshAffiliateWidgetsOnSave = () => {
       refreshAffiliateWidgets();
     }
   };
-  // A hacky backup in case whenEditorIsReady fails.
-  setTimeout(() => refreshWidgetsIfNecessary(), 2000);
   // The below seems to work consistently Chromium browsers.
   whenEditorIsReady().then(() => refreshWidgetsIfNecessary());
 };
