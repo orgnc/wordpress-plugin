55493efbd76980250478bb2aa60a3ca2913afd4c
Sergey Anuchin <sergunich@gmail.com>
2023-05-23 16:50:20 +0300

    EP-4833: get rid of config fields used only for SDKv1

--- src/Organic/AdsConfig.php
+++ src/Organic/AdsConfig.php
@@ -24,24 +24,17 @@ class AdsConfig extends BaseConfig {
      */
     public $forPlacement;
 
-    public $adsRefreshRates;
-
     private $fallbackUrl;
     private $prebidUrl;
     private $MODERN_PREBID_RE = '/.*sdk\/(prebid|prebid-stable|prebid-canary)\.js/';
 
-    public function __construct( array $rawAdsConfig, array $rawAdsRefreshRates ) {
+    public function __construct( array $rawAdsConfig ) {
         parent::__construct( $rawAdsConfig );
 
         $this->fallbackUrl = Organic::getInstance()->sdk->getFallbackPrebidBuildUrl();
 
-        $this->adsRefreshRates = $rawAdsRefreshRates;
-        if ( ! empty( $rawAdsRefreshRates ) ) {
-            $this->raw['adsRefreshRates'] = $rawAdsRefreshRates;
-        }
         if ( empty( $rawAdsConfig ) ) {
             $this->adRules = [];
-            $this->adsRefreshRates = [];
             $this->prebidUrl = $this->fallbackUrl;
             return;
         }
